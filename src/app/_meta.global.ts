7280cff8297ee9851b9ab5082454160300581170
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-03-30 16:07:33 -0700

    add installation guide

--- src/app/_meta.global.ts
+++ src/app/_meta.global.ts
@@ -2,6 +2,7 @@ import type { MetaRecord } from "nextra";
 
 const chapter1: MetaRecord = {
   "ch-01-01-installation": "1.1 Installation",
+  "ch-01-02-hello-world": "1.2 Hello, World!",
 };
 
 const meta = {
