1e897a41d9ddf7102ee3a63cbb31a9d9df086757
Michael Mkrtchyan <michaelmkrtch.dev@gmail.com>
2025-03-31 22:03:04 -0700

    add chapter 1.3 on building code

--- src/app/_meta.global.ts
+++ src/app/_meta.global.ts
@@ -3,6 +3,7 @@ import type { MetaRecord } from "nextra";
 const chapter1: MetaRecord = {
   "ch-01-01-installation": "1.1 Installation",
   "ch-01-02-hello-world": "1.2 Hello, World!",
+  "ch-01-03-zig-build": "1.3 The Zig Build System",
 };
 
 const meta = {
