29fe3f4b1da7e7db4f6ede4d6c3e1562f972dd0e
Michael <michaelmkrtch@gmail.com>
2025-08-13 17:35:18 -0700

    initialize Fumadocs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,88 +1,17 @@
-import { Analytics } from "@vercel/analytics/react";
+import '@/app/global.css';
+import { RootProvider } from 'fumadocs-ui/provider';
+import { Inter } from 'next/font/google';
+import type { ReactNode } from 'react';
 
-import localFont from "next/font/local";
-
-import { Layout, Navbar } from "nextra-theme-docs";
-import { Head, Search } from "nextra/components";
-import { getPageMap } from "nextra/page-map";
-import "nextra-theme-docs/style.css";
-import "./globals.css";
-
-import type { Metadata } from "next";
-import ZigIcon from "./components/icons/ZigIcon";
-import GithubIcon from "./components/icons/GithubIcon";
-
-const pretendard = localFont({
-  variable: "--font-pretendard",
-  src: "./fonts/pretendard-var.woff2",
-  display: "swap",
+const inter = Inter({
+  subsets: ['latin'],
 });
 
-const monaspace = localFont({
-  variable: "--font-monaspace",
-  src: "./fonts/monaspace-neon.woff2",
-  display: "swap",
-});
-
-export const metadata: Metadata = {
-  title: "The Zig Book",
-  description:
-    "Welcome to The Zig Book, your comprehensive guide to the Zig programming language. Follow this guide to master Zig and build efficient, reliable systems.",
-};
-
-// const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
-const navbar = (
-  <Navbar
-    className="h-[72px]!"
-    logo={
-      <div className="flex items-center gap-x-2.5">
-        <ZigIcon />
-        <h1 className="text-[28px] font-bold font-pretendard">The Zig Book</h1>
-      </div>
-    }
-    logoLink={true}
-    projectLink="https://github.com/MichaelMkrtch/zig-book"
-    projectIcon={<GithubIcon />}
-  />
-);
-
-export default async function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" dir="ltr" suppressHydrationWarning>
-      <Head />
-      <body
-        className={`${pretendard.variable} ${monaspace.variable} antialiased`}
-        style={{ margin: 0 }}
-      >
-        <Layout
-          // banner={banner}
-          search={<Search />}
-          navbar={navbar}
-          pageMap={await getPageMap()}
-          nextThemes={{
-            attribute: "class",
-            defaultTheme: "system",
-            disableTransitionOnChange: true,
-            storageKey: "theme",
-          }}
-          themeSwitch={{
-            dark: "Dark",
-            light: "Light",
-            system: "System",
-          }}
-          navigation={true}
-          sidebar={{ toggleButton: true }}
-          editLink={null}
-          feedback={{ content: null }}
-          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
-        >
-          {children}
-          <Analytics />
-        </Layout>
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <body className="flex flex-col min-h-screen">
+        <RootProvider>{children}</RootProvider>
       </body>
     </html>
   );
