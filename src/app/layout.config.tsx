8436cc913f4b95fb0c2d926aa99a8e6cd88da821
Michael <michaelmkrtch@gmail.com>
2025-08-13 20:17:12 -0700

    feat: add Zig logo

--- src/app/layout.config.tsx
+++ src/app/layout.config.tsx
@@ -1,4 +1,5 @@
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import Image from 'next/image';
 
 /**
  * Shared layout configurations
@@ -11,14 +12,12 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
+        <Image
+          src="/zig-logo.png"
+          alt="Zig logo. The letter 'Z' shaped like a bolt of lightning."
+          width={32}
+          height={32}
+        />
         The Zig Book
       </>
     ),
