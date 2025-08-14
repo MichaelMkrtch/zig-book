1d2bdc97193193a6594918ecb1da2f54e2548477
Michael <michaelmkrtch@gmail.com>
2025-08-13 18:27:12 -0700

    feat: add chapter 1-1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,28 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
+import { Geist, Geist_Mono } from 'next/font/google';
 import type { ReactNode } from 'react';
 
-const inter = Inter({
+const geist = Geist({
   subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-geist',
+});
+
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-geist-mono',
 });
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+    <html
+      lang="en"
+      className={`${geist.variable} ${geistMono.variable}`}
+      suppressHydrationWarning
+    >
+      <body className="flex flex-col min-h-screen font-geist">
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
